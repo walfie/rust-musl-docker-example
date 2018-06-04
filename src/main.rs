ffdd95fddf29abd37559969eb86705ee8346f516
Walfie <walfington@gmail.com>
2018-06-04 15:18:19 -0400

    Replace ProxyService with service_fn

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 extern crate hyper;
 extern crate hyper_tls;
 
-use hyper::client::ResponseFuture;
 use hyper::rt::Future;
-use hyper::service::Service;
-use hyper::{Body, Client, Request, Server, Uri};
+use hyper::service::service_fn;
+use hyper::{Body, Client, Request, Server};
 use hyper_tls::HttpsConnector;
 
 fn main() {
@@ -13,43 +12,29 @@ fn main() {
 
     let connector = HttpsConnector::new(1).expect("failed to create HttpsConnector");
 
-    let service = ProxyService {
-        client: Client::builder().build(connector),
-        base_uri: "https://httpbin.org".parse().unwrap(),
-    };
+    let client = Client::builder().build(connector);
 
     let server = Server::bind(&bind_address)
-        .serve(move || Ok::<_, hyper::Error>(service.clone()))
+        .serve(move || {
+            let cloned_client = client.clone();
+
+            service_fn(move |incoming: Request<Body>| {
+                let uri = format!("https://httpbin.org{}", incoming.uri().path())
+                    .parse()
+                    .expect("invalid URI");
+
+                let (mut parts, body) = incoming.into_parts();
+                parts.uri = uri;
+                parts.headers.remove(hyper::header::HOST);
+                let outgoing = Request::from_parts(parts, body);
+
+                println!("Sending: {:?}", outgoing);
+
+                cloned_client.request(outgoing)
+            })
+        })
         .map_err(|e| eprintln!("error: {}", e));
 
     println!("Server listening on {}", bind_address);
     hyper::rt::run(server);
-}
-
-#[derive(Clone)]
-struct ProxyService {
-    client: Client<HttpsConnector<hyper::client::HttpConnector>>,
-    base_uri: Uri,
-}
-
-impl Service for ProxyService {
-    type ReqBody = Body;
-    type ResBody = Body;
-    type Error = hyper::Error;
-    type Future = ResponseFuture;
-
-    fn call(&mut self, incoming: Request<Self::ReqBody>) -> Self::Future {
-        let uri = format!("{}{}", self.base_uri, incoming.uri().path())
-            .parse()
-            .expect("invalid URI");
-
-        let (mut parts, body) = incoming.into_parts();
-        parts.uri = uri;
-        parts.headers.remove(hyper::header::HOST);
-        let outgoing = Request::from_parts(parts, body);
-
-        println!("Sending: {:?}", outgoing);
-
-        self.client.request(outgoing)
-    }
 }
